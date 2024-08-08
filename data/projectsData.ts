bcc51a5ea64fbe0f7be0cc45f86f7fd2ec32efd2
Taeuk Kang <code@taeuk.net>
2024-08-08 00:10:57 -0700

    Update projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,5 +1,21 @@
 const projectsData = [
   {
+    title: "Oculife for Vision Pro, a TreeHacks 2024 project",
+    githubRepositoryName: "oculife",
+    description:
+      "An augmented reality app providing emergency medical response through interactive videos powered by text-to-speech and a large language model",
+    liveUrl: "https://devpost.com/software/oculife",
+    linkText: "Read the Devpost from TreeHacks 2024",
+  },
+  {
+    title: "Fine-tuning CodeLlama-7B on Synthetic Data for FORTRAN Code Generation with PEFT",
+    description:
+      "Received Outstanding Project Award in Stanford's CS224N: Natural Language Processing with Deep Learning",
+    linkText: "View research paper",
+    liveUrl: "https://web.stanford.edu/class/archive/cs/cs224n/cs224n.1244/final-projects/AndrewCShiSohamGovandeTaeukKang.pdf",
+    githubRepositoryName: "fortran-llm",
+  },
+  {
     title: "The Serendipitous Maestro",
     githubRepositoryName: "serendipitous-maestro",
     description:
@@ -14,14 +30,6 @@ const projectsData = [
     linkText: "View research poster",
   },
   {
-    title: "COVID-19 Korea Mask Inventory",
-    githubRepositoryName: "covid-19-mask-map",
-    description:
-      "A web app to help people find mask inventory in South Korea during the COVID-19 pandemic",
-    liveUrl: "https://mask.livecorona.co.kr/",
-    linkText: "Launch web app",
-  },
-  {
     title: "Google Home Push Notifications",
     githubRepositoryName: "google-home-push",
     description:
@@ -32,14 +40,9 @@ const projectsData = [
   {
     title: "California DMV Appointment Tracker",
     githubRepositoryName: "ca-dmv-appointment-bot",
-    description: "A bot that checks for available appointments at the California DMV",
-  },
-  {
-    title: "taeuk.net",
-    githubRepositoryName: "website",
-    description: "The repository for this website",
-    liveUrl: "https://www.taeuk.net",
-    linkText: "Back to homepage",
+    description: "A useful bot that checks for available appointments at the California DMV",
+    liveUrl: "https://github.com/taeukkang/ca-dmv-appointment-bot",
+    linkText: "View on GitHub",
   },
 ];
 

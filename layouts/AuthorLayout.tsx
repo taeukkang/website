9652b984e41c13a4cd9519eb27cd40b4394c618b
Taeuk Kang <code@taeuk.net>
2024-11-11 22:47:41 -0800

    Add X/Twitter link and update experiences

--- layouts/AuthorLayout.tsx
+++ layouts/AuthorLayout.tsx
@@ -32,6 +32,7 @@ export default function AuthorLayout({ children, content }: Props) {
               <SocialIconClient kind="emailEncoded" href={`${emailEncoded}`} />
               <SocialIcon kind="github" href={github} />
               <SocialIcon kind="linkedin" href={linkedin} />
+              <SocialIcon kind="twitter" href={twitter} />
             </div>
           </div>
           <div className="prose max-w-none pb-8 pt-8 dark:prose-invert xl:col-span-2">
